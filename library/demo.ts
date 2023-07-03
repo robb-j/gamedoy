cc82905134732ddffaf37360c055117e58e6a0d9
Rob Anderson <rob@andrsn.uk>
2023-07-03 20:24:59 +0100

    rework demo to embed snek

--- library/demo.ts
+++ library/demo.ts
@@ -1,71 +1,6 @@
-import {
-  bootScene,
-  create2dCanvas,
-  css,
-  Gamedoy,
-  html,
-  Runtime,
-} from './mod.js'
+import { bootScene, Gamedoy, iframeScene, Scene } from './mod.js'
 
-const helloWorld = {
-  setup({ disposables, setDisplay, finish, controls }: Runtime) {
-    const canvas = create2dCanvas(400, 400)
-
-    disposables.add(
-      setDisplay(canvas.elem),
-      controls.onKeyUp('A', () => finish())
-    )
-
-    canvas.ctx.font = '42px Pixeboy'
-    canvas.ctx.textAlign = 'center'
-    canvas.ctx.fillStyle = 'white'
-    canvas.ctx.fillText('Hello, world!', 200, 200)
-  },
-}
-
-const listStyle = css`
-  .listScene > * + * {
-    padding-top: 0.5em;
-    border-top: 2px solid black;
-    margin-top: 0.5em;
-  }
-  .listScene p {
-    margin-bottom: 0;
-    font-size: 16px;
-    text-align: center;
-  }
-`
-
-const listTemplate = document.createElement('template')
-listTemplate.innerHTML = html`
-  <style>
-    ${listStyle}
-  </style>
-  <div class="listScene">
-    <p>Option A</p>
-    <p>Option B</p>
-    <p>Option C</p>
-    <p>Option D</p>
-    <p>Option E</p>
-    <p>Option F</p>
-    <p>Option G</p>
-  </div>
-`
-
-const list = {
-  setup({ disposables, setDisplay, finish }: Runtime<null, string>) {
-    const elem = document.createElement('div')
-    elem.append(listTemplate.content.cloneNode(true))
-
-    disposables.add(setDisplay(elem))
-
-    for (const p of elem.querySelectorAll<HTMLParagraphElement>('p')) {
-      p.onclick = () => {
-        finish(p.textContent ?? 'unknown')
-      }
-    }
-  },
-}
+type IframeScene = Scene<null, undefined, { score: number }>
 
 async function main() {
   const gamedoy = Gamedoy.setup({ el: 'gamedoy-console' })
@@ -74,11 +9,15 @@ async function main() {
     version: 'v0.1.2',
   })
 
-  await gamedoy.run(helloWorld)
+  const scene = iframeScene('https://snake.andrsn.uk/', {
+    width: 400,
+    height: 400,
+  }) as IframeScene
 
-  const answer = await gamedoy.run(list)
-
-  alert('You chose:' + answer)
+  while (true) {
+    const result = await gamedoy.run(scene)
+    alert('Game over, you scored: ' + result.score)
+  }
 }
 
 main()
